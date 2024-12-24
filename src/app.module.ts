40100545b276df6843142ecc6b997eb083a33de2
NicolasMezieres <nicolas.mezieres73@gmail.com>
2024-12-24 03:55:54 +0100

    Create auth/email

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]),
+    AuthModule,
+    PrismaModule,
+    EmailModule,
+  ],
 })
 export class AppModule {}
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { signinDTO, signupDTO } from './dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("/signup")
+  signup(@Body() dto:signupDTO){
+    return this.authService.signup(dto);
+  }
+
+  @Post("/signin")
+  signin(@Body() dto:signinDTO){
+    return this.authService.signin(dto);
+  }
+  @Patch("/activate/:token")
+  activateAccount(@Param("token") token:string){
+    return this.authService.activateAccount(token);
+  }
+}
