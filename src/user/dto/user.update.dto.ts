77177060e6cc1e883063af2f01df3b085b77dd11
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-01-04 03:35:35 +0100

    documentation dto ressource user

--- src/user/dto/user.update.dto.ts
+++ src/user/dto/user.update.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsBoolean,
   IsEmail,
@@ -7,30 +8,38 @@ import {
 } from 'class-validator';
 
 export class updateUserDTO {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MaxLength(20)
   userName: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MaxLength(30)
   lastName: string;
+
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MaxLength(30)
   firstName: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MaxLength(20)
   phone: string;
+
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MaxLength(320)
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsBoolean()
   @IsNotEmpty()
   isActive: boolean;
