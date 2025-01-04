cb64d4b3e0af9b67b1bab7825147ce8737ef61ad
NicolasMezieres <nicolas.mezieres73@gmail.com>
2025-01-04 02:54:25 +0100

    Reconfigure throttler for rate limiting, install swagger and document dto auth

--- src/auth/dto/signin.dto.ts
+++ src/auth/dto/signin.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class signinDTO {
-
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
-  identifiant: string
+  identifiant: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
